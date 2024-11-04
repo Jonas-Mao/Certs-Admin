de80821dde50ae2e8d6bf1514d5a85c8c4a4f0a9
Lins <161346037+Jonas-Mao@users.noreply.github.com>
2024-11-04 16:30:52 +0800

    Update views.py

--- system/views.py
+++ system/views.py
@@ -42,84 +42,7 @@ class SystemViewSet(ModelViewSet):
             return [permissions.IsAdminUser()]
         return [permissions.IsAuthenticated()]
 
-    def create(self, request, *args, **kwargs):
-        """
-        首次创建
-        """
-        serializer = self.get_serializer(data=request.data)
-        try:
-            serializer.is_valid(raise_exception=True)
-        except ValidationError as e:
-            error = json.dumps(e.detail, ensure_ascii=False)
-            error = re.sub(r'[\[\]{}"\s]', '', error)   # 替换[]、{}、"和空格
-            res = {'code': e.status_code, 'msg': error}
-            return Response(res)
-
-        mail_host = request.data.get('mail_host')
-        mail_port = request.data.get('mail_port')
-        mail_alias = request.data.get('mail_alias')
-        mail_username = request.data.get('mail_username')
-        mail_password = request.data.get('mail_password')
-        scheduler_cron = request.data.get('scheduler_cron')
-
-        data = [
-            {
-                'key': ConfigKeyEnum.MAIL_HOST,
-                'value': mail_host,
-                'label': '发件邮箱服务器地址',
-                'placeholder': '发件邮箱服务器地址',
-                'is_show_value': True,
-            },
-            {
-                'key': ConfigKeyEnum.MAIL_PORT,
-                'value': mail_port,
-                'label': '发件邮箱服务器端口',
-                'placeholder': '发件邮箱服务器端口：25 或者 465(ssl)',
-                'is_show_value': True,
-            },
-            {
-                'key': ConfigKeyEnum.MAIL_ALIAS,
-                'value': mail_alias,
-                'label': '发件人邮箱名称',
-                'placeholder': '发件人邮箱名称',
-                'is_show_value': True,
-            },
-            {
-                'key': ConfigKeyEnum.MAIL_USERNAME,
-                'value': mail_username,
-                'label': '发件人邮箱账号',
-                'placeholder': '发件人邮箱账号',
-                'is_show_value': True,
-            },
-            {
-                'key': ConfigKeyEnum.MAIL_PASSWORD,
-                'value': mail_password,
-                'label': '发件人邮箱密码',
-                'placeholder': '发件人邮箱密码或客户端授权码',
-                'is_show_value': True,
-            },
-            {
-                'key': ConfigKeyEnum.SCHEDULER_CRON,
-                'value': scheduler_cron,
-                'label': '定时检测时间（crontab 表达式）',
-                'placeholder': '分 时 日 月 周',
-                'is_show_value': True,
-            }
-        ]
-
-        objects = [
-            System(
-                key=item['key'],
-                value=item['value'],
-                is_show_value=item['is_show_value'],
-                label=item['label'],
-                placeholder=item['placeholder']
-            ) for item in data]
-
-        System.objects.bulk_create(objects)
-
-
-# ******
+    
 @auth_service.permission(role=RoleEnum.USER)
 def update_mail_conf(request):
     """
@@ -146,7 +69,6 @@ def update_mail_conf(request):
     return JsonResponse(res)
 
 
-# ******
 @auth_service.permission(role=RoleEnum.ADMIN)
 def update_cron_conf(request):
     """
@@ -160,14 +82,12 @@ def update_cron_conf(request):
         update_time=datetime_util.get_datetime()
     )
 
-    # 更新定时
     scheduler_service.update_job(scheduler_cron)
 
     res = {'code': 200, 'msg': '更新成功！'}
     return JsonResponse(res)
 
 
-# ******
 def handle_mail_test(request):
     """
     测试邮件
@@ -204,7 +124,6 @@ def handle_mail_test(request):
     return JsonResponse(res)
 
 
-# ******
 def get_monitor_task_next_run_time():
     return {
         'next_run_time': scheduler_main.get_monitor_task_next_run_time()
