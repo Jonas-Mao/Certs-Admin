e8a953fe971aa2ef67e444bc6ba169b69a35b60c
Lins <161346037+Jonas-Mao@users.noreply.github.com>
2024-11-04 16:05:52 +0800

    Update email_util.py

--- certs_admin/utils/email_util.py
+++ certs_admin/utils/email_util.py
@@ -33,49 +33,29 @@ def send_email(
         mail_host=DEFAULT_MAIL_HOST, mail_port=25, content_type='plain',
         mail_alias=None, mail_username=None, mail_password=None
 ):
-    """
-    :param mail_host: 主机地址
-    :param mail_port: 端口
-    :param mail_alias: 发件人别名
-    :param subject: 主题
-    :param content: 内容
-    :param to_addresses: 收件人列表
-    :param mail_username: 发件人账号
-    :param mail_password: 发件人密码
-    :param content_type: 内容类型
-        - plain: 文本
-        - html: 富文本
-    """
     mail_username = mail_username or DEFAULT_MAIL_USERNAME
     mail_alias = mail_alias or mail_username
 
-    # 构造邮件
     message = MIMEText(content, content_type, 'utf-8')
-    # 邮箱主题
     message['Subject'] = Header(subject, 'utf-8')
-    # 昵称,发件人邮箱
     message['From'] = formataddr((mail_alias, mail_username))
-    # 显示收件人
     message['To'] = ','.join(formataddr((mail, mail)) for mail in to_addresses)
-
+    
     server = get_email_server(mail_host, mail_port)
 
-    # 需要登录，否则匿名
     if mail_password:
         try:
             server.ehlo(mail_host)
             server.login(mail_username, mail_password)
         except Exception as e:
             print("连接失败：%s" %e)
-
-    # 发送
+            
     server.sendmail(
         from_addr=mail_username,
         to_addrs=to_addresses,
         msg=message.as_string()
     )
 
-    # 退出
     server.quit()
 
     return JsonResponse(
